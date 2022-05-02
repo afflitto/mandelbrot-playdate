3ea6faaea5e78f4651bfb15e2f0429a4c4661110
Andrew <andrew@thotpad.localdomain>
2022-05-02 02:07:21 -0400

    add zoom functionality

--- Source/main.lua
+++ Source/main.lua
@@ -7,16 +7,20 @@ import "tile"
 local gfx <const> = playdate.graphics
 local counter = 0
 local tiles = {}
-local min_x <const> = -2.00
-local max_x <const> = 0.47
-local min_y <const> = -1.12
-local max_y <const> = 1.12
+local min_x = -2.00
+local max_x = 0.47
+local min_y = -1.12
+local max_y = 1.12
+local offset_x = 0
+local offset_y = 0
+local move_step = 0.05
+local zoom_step <const> = 0.9
 local tiles_x <const> = 16
 local tiles_y <const> = 16
 local tile_width <const> = playdate.display.getWidth() / tiles_x	
 local tile_height <const> = playdate.display.getHeight() / tiles_y	
 
-local max_iteration = 100
+local max_iteration = 50
 local show_debug = false
 local slowest_elapsed = 0
 
@@ -27,50 +31,22 @@ function setup()
 	playdate.graphics.setColor(playdate.graphics.kColorWhite)
 	playdate.display.setScale(1)
 
-	local scaled_range_x = (max_x - min_x) / tiles_x
-	local scaled_range_y = (max_y - min_y) / tiles_y
-	print("i, j, tile_min_x, tile_max_x, tile_min_y, tile_max_y")
-
-	for i=0,(tiles_x-1) do
-		for j=0,(tiles_y-1) do
-			local tile_min_x = scaled_range_x * i + min_x
-			local tile_max_x = scaled_range_x * (i + 1) + min_x
-			local tile_min_y = scaled_range_y * j + min_y
-			local tile_max_y = scaled_range_y * (j + 1) + min_y
-			local tile = Tile(
-				tile_min_x, 
-				tile_max_x, 
-				tile_min_y, 
-				tile_max_y, 
-				tile_width,
-				tile_height
-			)
-			-- tile:render()
-			tiles[i * tiles_x + j + 1] = tile
-			print(i, j, tile_min_x, tile_max_x, tile_min_y, tile_max_y)
-		end
-	end
-
-	print("testing C call")
-	c_test()
+	create_tiles()
 end
-setup()
 
 function playdate.update()
-	-- local idx = math.floor(counter / 1) % (tiles_x * tiles_y)
-	-- local tile = tiles[idx + 1]
-	-- local x, y = index_to_xy(idx)
+	create_tiles()
+
 	local x = 0
 	local y = 0
 
 	playdate.resetElapsedTime()
-	-- tile:render(max_iteration)
-	-- tile:crender(max_iteration)
-	-- tile.buffer:draw(x, y)
 	for i=1,(tiles_x*tiles_y) do
 		local tile = tiles[i]
 		x, y = index_to_xy(i)
-		tile:crender(max_iteration)
+		if counter % tiles_y == i % tiles_x then
+			tile:crender(max_iteration)
+		end
 		tile.buffer:draw(x, y)
 	end
 
@@ -85,27 +61,77 @@ function playdate.update()
 	gfx.fillRect(0, 0, 150, 20)
 	gfx.setColor(gfx.kColorBlack)	
 	if show_debug then
-		-- Draw max_iteration text
 		gfx.drawText(string.format("max__iteration=%d", max_iteration), 0, 0)
-
-		playdate.drawFPS(0, playdate.display.getHeight()-10)
 	else
 		gfx.drawText(" *mandelbr0t*", 0, 0)
 	end
+	playdate.drawFPS(0, playdate.display.getHeight()-10)
 end
 
 function index_to_xy(idx)
-	local x = math.floor(idx / tiles_x) * tile_width
-	local y = (idx % tiles_y) * tile_height
+	local x = math.floor((idx - 1)/ tiles_x) * tile_width
+	local y = ((idx - 1) % tiles_y) * tile_height
 	return x, y
 end
 
-function playdate.downButtonUp()
-	max_iteration -= 1
+function create_tiles()
+	local scaled_range_x = (max_x - min_x) / tiles_x
+	local scaled_range_y = (max_y - min_y) / tiles_y
+
+	for i=0,(tiles_x-1) do
+		for j=0,(tiles_y-1) do
+			local tile_min_x = scaled_range_x * i + min_x + offset_x
+			local tile_max_x = scaled_range_x * (i + 1) + min_x + offset_x
+			local tile_min_y = scaled_range_y * j + min_y + offset_y
+			local tile_max_y = scaled_range_y * (j + 1) + min_y + offset_y
+			local tile = Tile(
+				tile_min_x, 
+				tile_max_x, 
+				tile_min_y, 
+				tile_max_y, 
+				tile_width,
+				tile_height
+			)
+			tiles[i * tiles_x + j + 1] = tile
+		end
+	end
+end
+
+-- function playdate.downButtonUp()
+-- 	max_iteration -= 1
+-- end
+-- function playdate.upButtonUp()
+-- 	max_iteration += 1
+-- end
+function playdate.leftButtonUp()
+	offset_x -= move_step
+	offset_x -= move_step
+end
+function playdate.rightButtonUp()
+	offset_x += move_step
+	offset_x += move_step
 end
 function playdate.upButtonUp()
-	max_iteration += 1
+	offset_y -= move_step
+	offset_y -= move_step
+end
+function playdate.downButtonUp()
+	offset_y += move_step
+	offset_y += move_step
 end
 function playdate.AButtonUp()
-	show_debug = not show_debug
+	min_x *= zoom_step
+	min_y *= zoom_step
+	max_x *= zoom_step
+	max_y *= zoom_step
+	move_step *= zoom_step
 end
+function playdate.BButtonUp()
+	min_x /= zoom_step
+	min_y /= zoom_step
+	max_x /= zoom_step
+	max_y /= zoom_step
+	move_step /= zoom_step
+end
+
+setup()
